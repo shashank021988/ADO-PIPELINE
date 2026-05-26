370b5d6f1b6ad1ed783ba804a5b0f12252412b9b
shashank021988 <shashank021988@gmail.com>
2026-05-27 00:19:52 +0530

    Added first text file

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "testrg" {
+  name     = "testrg"
+  location = "eastus"       
+  
+}
+
+
+resource "azurerm_storage_account" "testsa" {
+  name                     = "testsa"
+  resource_group_name      = azurerm_resource_group.testrg.name
+  location                 = azurerm_resource_group.testrg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}           
+
+
